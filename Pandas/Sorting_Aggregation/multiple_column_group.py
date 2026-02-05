7d5442ef558c73ee44695bb19ae620e7e0001051
rep-demo <aashishmijar44@gmail.com>
2026-02-05 20:07:27 +0545

    Groupby multiple column

---
+++ Pandas/Sorting_Aggregation/multiple_column_group.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+data ={
+    "Name": ["Arun", "Varun", "Karan", "Tarun", "Sagar", "Naran"],
+    "Age": [30, 40, 30, 35, 30, 24],
+    "Salary": [10000, 30000, 20000, 50000, 60000, 35000]
+}
+
+df = pd.DataFrame(data)
+
+grouped = df.groupby(["Age", "Name"])["Salary"].sum()
+print(grouped)
+
+"""
+1-sum()
+2-mean()
+3-count()
+4-min()
+5-max()
+6-std()
+"""
