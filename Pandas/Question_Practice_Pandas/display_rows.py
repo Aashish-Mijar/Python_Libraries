dab3ece05f57267b20e7934afe4ecbb1717a2981
rep-demo <aashishmijar44@gmail.com>
2026-02-09 20:56:40 +0545

    Displaying rows and columns

--- Pandas/Question_Practice_Pandas/display_rows.py
+++ Pandas/Question_Practice_Pandas/display_rows.py
@@ -4,3 +4,7 @@ df = pd.read_csv("Pandas\Question_Practice_Pandas\example_sales.csv")
 
 print(df.head())
 print(df.tail())
+
+rows, columns = df.shape
+print("Rows ",rows)
+print("Columns ", columns)
