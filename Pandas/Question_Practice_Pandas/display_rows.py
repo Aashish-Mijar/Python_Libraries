0744d08a65f362a56de02271d23560616650b811
rep-demo <aashishmijar44@gmail.com>
2026-02-09 20:58:09 +0545

    Displaying columns and datatypes

--- Pandas/Question_Practice_Pandas/display_rows.py
+++ Pandas/Question_Practice_Pandas/display_rows.py
@@ -5,6 +5,12 @@ df = pd.read_csv("Pandas\Question_Practice_Pandas\example_sales.csv")
 print(df.head())
 print(df.tail())
 
+# Displaying rows and columns number
 rows, columns = df.shape
 print("Rows ",rows)
 print("Columns ", columns)
+
+# Displaying column names and data types
+
+print(df.columns)
+print(df.dtypes)
