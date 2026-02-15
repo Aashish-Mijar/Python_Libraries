5a10f8056211e1b15adb7cecc6a15ef7e1d1ed17
rep-demo <aashishmijar44@gmail.com>
2026-02-15 21:25:58 +0545

    Horizontal concatenation

--- Pandas/Question_Practice_Pandas/concatenate_dataFrames.py
+++ Pandas/Question_Practice_Pandas/concatenate_dataFrames.py
@@ -6,3 +6,7 @@ df_b = pd.DataFrame({"A":[6,8], "B":[7, 2]})
 # Vertical concatenation
 vertical = pd.concat([df_a, df_b])
 print(vertical)
+
+# Horizontal concatenation
+horizontal = pd.concat([df_a, df_b], axis = 1)
+print(horizontal)
