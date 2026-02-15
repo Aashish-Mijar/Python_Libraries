3b3147c964db6108fd8d1e5d9c4bfcf0211306b7
rep-demo <aashishmijar44@gmail.com>
2026-02-15 21:24:36 +0545

    Vertical Concatenation

--- Pandas/Question_Practice_Pandas/concatenate_dataFrames.py
+++ Pandas/Question_Practice_Pandas/concatenate_dataFrames.py
@@ -2,3 +2,7 @@ import pandas as pd
 
 df_a = pd.DataFrame({"A":[1,3], "B":[5,6]})
 df_b = pd.DataFrame({"A":[6,8], "B":[7, 2]})
+
+# Vertical concatenation
+vertical = pd.concat([df_a, df_b])
+print(vertical)
