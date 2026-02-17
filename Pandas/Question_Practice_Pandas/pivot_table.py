6a07d2c50a1c62b68acccb4ef2f695315598c8eb
rep-demo <aashishmijar44@gmail.com>
2026-02-17 20:27:50 +0545

    Convet categorical data using map()

--- Pandas/Question_Practice_Pandas/pivot_table.py
+++ Pandas/Question_Practice_Pandas/pivot_table.py
@@ -10,4 +10,8 @@ df = pd.DataFrame(data)
 
 pivot = pd.pivot_table(df, values="Marks", index="Class", aggfunc="mean")
 
-print(pivot)
+# print(pivot)
+
+# map() for categorical conversion
+df["Grade"]= df["Marks"].map(lambda x: "Pass" if x>=40 else "Fail")
+# print(df["Grade"])
