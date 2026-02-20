44e6c3148fd9baf465d703cb552a6cf219f1315e
rep-demo <aashishmijar44@gmail.com>
2026-02-20 19:57:24 +0545

    Creating random array

--- NumPyPractice/Question_Practice/NumpyArray.py
+++ NumPyPractice/Question_Practice/NumpyArray.py
@@ -6,3 +6,7 @@ print(arr)
 # creating 3 * 3 matrix filled with zeros
 matrix_zeros = np.zeros((3,3))
 print(matrix_zeros)
+
+# Creating array of 5 random integers
+random_arr = np.random.randint(1, 101, 5)
+print(random_arr)
