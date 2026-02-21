d4f6bc40b67a5fab2a6dd74de76f8108195f7f3a
rep-demo <aashishmijar44@gmail.com>
2026-02-21 20:11:36 +0545

    operation in array

---
+++ NumPyPractice/Question_Practice/operation_array.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+a = np.array([10, 30, 40])
+b = np.array([1, 4, 6])
+
+print("Addition: ", a + b)
+print("Subtraction: ", a - b)
+print("Multiplication: ", a * b)
+print("Division: ", a / b)
