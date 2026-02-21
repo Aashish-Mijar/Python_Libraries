a90fb0a7e25733d0f78e65dc57b7b2faa50fbbfa
rep-demo <aashishmijar44@gmail.com>
2026-02-21 20:19:40 +0545

    Stacking array

---
+++ NumPyPractice/Question_Practice/stack_array.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+a = np.array([1,3,4])
+b = np.array([6,7,8])
+
+# Vertical stacking
+vertical = np.vstack((a, b))
+
+# Horizontal stacking
+horizontal = np.hstack((a, b))
+
+print("Vertical:\n", vertical)
+print("Horizontal:\n", horizontal)
