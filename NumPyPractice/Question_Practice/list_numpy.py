b9ba7a2324e1f8b105f7d9786c4aab85845fd1c3
rep-demo <aashishmijar44@gmail.com>
2026-02-24 20:45:37 +0545

    Python list vs Numpy Array

---
+++ NumPyPractice/Question_Practice/list_numpy.py
@@ -0,0 +1,15 @@
+import numpy as np
+import time
+
+# Python time
+list_data = list(range(1000000))
+start = time.time()
+list_result = [x * 2 for x in list_data]
+print("List Time: ", time.time() - start)
+
+
+#  Numpy array
+array_data = np.arange(1000000)
+start = time.time()
+array_result = array_data * 2
+print("Numpy Time: ", time.time() - start)
