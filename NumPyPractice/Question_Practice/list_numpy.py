3695ff4f409e7a70aeb296a03d4e09fb9fe91a4d
rep-demo <aashishmijar44@gmail.com>
2026-02-27 21:15:55 +0545

    Removed error part

--- NumPyPractice/Question_Practice/list_numpy.py
+++ NumPyPractice/Question_Practice/list_numpy.py
@@ -11,6 +11,5 @@ print("List Time: ", time.time() - start)
 #  Numpy array
 array_data = np.arange(1000000)
 start = time.time()
-start = time.time()
 array_result = array_data * 2
 print("Numpy Time: ", time.time() - start)
